d3bd4983e7020010e7fc576684276e019c09cbc2
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-16 00:07:03 +0900

    photo color picker

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-//var server = 'http://fxos-ps.azurewebsites.net';
-var server = 'http://localhost:8000';
+var server = 'http://fxos-ps.azurewebsites.net';
+//var server = 'http://localhost:8000';
 var api = server + '/api/photos';
 var apiget = server + '/api/myphotos/';
+var constrainedWidth = 240;
+var constrainedHeight = 320;
 
-var $$ = function(selector) {
-  return document.querySelector(selector);
-}
+var $$ = function(selector) {return document.querySelector(selector); }
+
 var photoService = {
   save: function(photo_data, callback) {
     $.ajax({
@@ -27,36 +28,87 @@ var photoService = {
 };
 
 var cnvCtrl = {
-  drowImg: function(video) {
-    cnvCtrl.ctx.drawImage(video, 0, 0, cnvCtrl.canvas.width, cnvCtrl.canvas.height);
+  /** CONTRACTOR **/
+  init: function(){
+    cnvCtrl.canvas = $$('#camera');
+    cnvCtrl.ctx    = cnvCtrl.canvas.getContext('2d');
+    cnvCtrl.video.element = $$('#camera_video');
+    cnvCtrl.resize();
+    screen.onmozorientationchange = cnvCtrl.resize;
+  },
+  /** CANVAS **/
+  drowImg: function(img) {
+    cnvCtrl.ctx.drawImage(img, 
+      0, 0, cnvCtrl.canvas.width, cnvCtrl.canvas.height);
   },
   createImage: function(base64) {
     var img = document.createElement('img');
     img.setAttribute('src', base64);
     return img;
-
+  },
+  resize: function(e){
+    // 320 x 240
+    cnvCtrl.canvas.width  = constrainedWidth;
+    cnvCtrl.canvas.height = constrainedHeight;
+    cnvCtrl.video.element.width  = constrainedWidth;
+    cnvCtrl.video.element.height = constrainedHeight;
+  },
+  isPortrait: function(){ // 縦方向だとtrue
+    console.log( screen.orientation)
+    return screen.mozOrientation === 'portrait';
+  },
+  /** SHOW CONTROLLER **/
+  show:{
+    video:function(){
+      $$('#camera_video').className = 'show';
+      $$('#camera').className = 'hide';
+    },
+    canvas:function(){
+      $$('#camera_video').className = 'hide';
+      $$('#camera').className = 'show';
+    }
+  },
+  /** VIDEO **/
+  video:{
+    element:null,
+    MediaStream:null,
+    sucess: function(localMediaStream){
+      cnvCtrl.video.MediaStream = localMediaStream;
+//       cnvCtrl.video.element.onloadedmetadata = function(e) {
+//         cnvCtrl.video.element.play();
+//       };
+      // ううん・・・firefox os で onloadedmetadata が動かない？
+      setTimeout(function(){
+        cnvCtrl.video.element.play();
+      },1000);
+      cnvCtrl.video.element.src = window.URL.createObjectURL(localMediaStream);
+      cnvCtrl.video.element.addEventListener("click", function shot() {
+        console.log('on click');
+        try{
+        cnvCtrl.drowImg(cnvCtrl.video.element);
+      }catch(e){ console.log('done drow', e); }
+        
+        cnvCtrl.video.MediaStream.stop();
+        cnvCtrl.show.canvas();
+        cnvCtrl.video.element.removeEventListener("click", shot);
+      });
+    }
   }
 };
-cnvCtrl.canvas = (function() {
-  return document.getElementById('camera')
-})();
-cnvCtrl.ctx = (function() {
-  return cnvCtrl.canvas.getContext('2d')
-})();
-cnvCtrl.canvas.width = 320;
-cnvCtrl.canvas.height = 240;
-
+cnvCtrl.init();
 
 $(function() {
 
   $('#imageSave').click(function() {
+    // ユーザー名とタイトルは必須！
     photoService.save(
       {
         img: cnvCtrl.canvas.toDataURL(),
-        usr: $('#username').val()
-      }, function(d) {
-      console.log('sucess!', d)
-    });
+        title: $('#username').val(),
+        usr: $('#title').val()
+      }, function(data) {
+        console.log('sucess!', data);
+      });
   });
 
   $('#imageLoad').click(function() {
@@ -78,29 +130,23 @@ $(function() {
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia);
 
+  var video_constraints = {
+    "mandatory": {
+      "minWidth": constrainedWidth,
+      "minHeight": constrainedHeight,
+      "minFrameRate": "30"
+    },
+    "optional": []
+  };
 
   $('#shutter').click(function() {
     if (navigator.getUserMedia) {
+      cnvCtrl.show.video();
       navigator.getUserMedia({
-          video: true,
+          video: video_constraints,
           audio: false
         },
-        function(localMediaStream) {
-          var video = document.querySelector('video');
-          video.src = window.URL.createObjectURL(localMediaStream);
-          video.width = cnvCtrl.canvas.width;
-          video.height = cnvCtrl.canvas.height;
-          video.onloadedmetadata = function(e) {
-            console.log(e, video)
-            video.play();
-          };
-
-          cnvCtrl.canvas.addEventListener("click", function shot() {
-            cnvCtrl.drowImg(video);
-            localMediaStream.stop();
-            cnvCtrl.canvas.removeEventListener("click", shot);
-          });
-        },
+        cnvCtrl.video.sucess,
         function(err) {
           console.log("The following error occured: " + err);
         }
