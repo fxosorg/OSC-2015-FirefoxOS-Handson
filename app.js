38055098cbeb4e047e323153f55acc0dee48bbdd
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-27 00:57:55 +0900

    camera api

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var FxOSApp = {
     FxOSApp.canvas = $$('#canvas');
     FxOSApp.ctx    = FxOSApp.canvas.getContext('2d');
     FxOSApp.video.element = $$('#camera_video');
+    FxOSApp.camera.element = $$('#camera');
     FxOSApp.resize();
+
   },
   /** データの保存 **/
   service:{
@@ -33,6 +35,10 @@ var FxOSApp = {
           title: $$('#title').value,
           usr:   $$('#username').value
         }));
+      var notification = new Notification('サーバへ保存しました。',{
+        body: 'インターネットの世界へ送り出しました。',
+        icon: window.location.origin + '/icons/firefox-16.png'
+      });
     },
   },
   /** キャンバスとカメラ入力の操作 **/
@@ -54,6 +60,27 @@ var FxOSApp = {
     canvas:function(){
       $$('#camera_video').className = 'hide';
       $$('#canvas').className = 'show';
+    }
+  },
+  /** カメラの操作 **/
+  camera:{
+    shutter: function(){
+      var camera = $$('#camera');
+      camera.onchange = function (event) {
+        // 撮影された写真または選択された画像への参照を取得
+        var files = event.target.files;
+        if (files && files.length > 0) {
+          var file = files[0];
+          var img = $$$('img');
+          img.onload = function(){
+            FxOSApp.drowImg(img);
+          };
+          var dataURL = URL.createObjectURL(file);
+          img.src = dataURL;
+          URL.revokeObjectURL(dataURL);
+        }
+      }
+      camera.click(); 
     }
   },
   /** ビデオ要素関係の操作 **/
@@ -91,6 +118,29 @@ var FxOSApp = {
     error: function(err) {
       console.log("The following error occured: ", err);
     }
+  },
+  userproximity: function(event){
+    FxOSApp.video.shutter();
+  },
+  devicelight: function(event){
+    // 光は0〜500を想定
+    var rgbRight = ((event.value>=500)?500:event.value) / 2;
+    var palet = $$('.pickval').value || "#000000";
+    var color = {
+      r:parseInt(palet.substr(1,2),16),
+      g:parseInt(palet.substr(3,2),16),
+      b:parseInt(palet.substr(5,2),16),
+      value:()=>{
+        return '#'
+             + ("0" + color.r.toString(16)).slice (-2)
+             + ("0" + color.g.toString(16)).slice (-2)
+             + ("0" + color.b.toString(16)).slice (-2)
+      }
+    };
+    color.r = parseInt(rgbRight);
+    color.g = parseInt(rgbRight);
+    color.b = parseInt(rgbRight);
+    $('.colorpick').colorpicker('setValue', color.value())
   }
 };
 
@@ -98,9 +148,12 @@ var onload_func = function() {
 
   FxOSApp.init();
 
+  //window.addEventListener('userproximity', FxOSApp.userproximity);
+  //window.addEventListener('devicelight',FxOSApp.devicelight);
+
   $$('#imageSave').addEventListener('click', FxOSApp.service.save, false);
 
-  $$('#shutter').addEventListener('click', FxOSApp.video.shutter, false);
+  $$('#shutter').addEventListener('click', FxOSApp.camera.shutter, false);
 
 };
 document.addEventListener('DOMContentLoaded', onload_func, false);
